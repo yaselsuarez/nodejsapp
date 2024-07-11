fecba2f09a437fd8a5d24d07365d603a7a956fd1
Yasel Suárez <yaselsuarez@gmail.com>
2024-07-11 09:52:03 +0000

    commit inicial

---
+++ DSL/nodejsDSL.groovy
@@ -0,0 +1,37 @@
+job('Aplicacion Node.js DSL') {
+    description('Aplicación Node JS DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/yaselsuarez/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('yaselsuarez')
+            node / gitConfigEmail('yaselsuarez@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell("npm install")
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
